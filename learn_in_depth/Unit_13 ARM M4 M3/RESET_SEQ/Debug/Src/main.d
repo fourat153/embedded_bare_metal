d872732e109554899450434ff11345d020af6694
fourat153 <ahmedchammam30@gmail.com>
2024-01-06 17:01:26 +0100

    reset seq

---
+++ learn_in_depth/Unit_13 ARM M4 M3/RESET_SEQ/Debug/Src/main.d
@@ -0,0 +1,9 @@
+Src/main.o: ../Src/main.c \
+ ../Src/../stm_driver/inc/Stm32_F103C6_EXTI_driver.h \
+ ../Src/../stm_driver/inc/stm32f103c6.h \
+ ../Src/../stm_driver/inc/Stm32_F103C6_gpio_driver.h \
+ ../Src/../stm_driver/inc/Stm32_F103C6_gpio_driver.h
+../Src/../stm_driver/inc/Stm32_F103C6_EXTI_driver.h:
+../Src/../stm_driver/inc/stm32f103c6.h:
+../Src/../stm_driver/inc/Stm32_F103C6_gpio_driver.h:
+../Src/../stm_driver/inc/Stm32_F103C6_gpio_driver.h:
---
+++ learn_in_depth/Unit_13 ARM M4 M3/RESET_SEQ/Debug/stm_driver/EXTI/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../stm_driver/EXTI/Stm32_F103C6_EXTI_driver.c 
+
+OBJS += \
+./stm_driver/EXTI/Stm32_F103C6_EXTI_driver.o 
+
+C_DEPS += \
+./stm_driver/EXTI/Stm32_F103C6_EXTI_driver.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+stm_driver/EXTI/%.o stm_driver/EXTI/%.su stm_driver/EXTI/%.cyclo: ../stm_driver/EXTI/%.c stm_driver/EXTI/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -c -I"C:/Users/mm77/Desktop/lean/embedded_diplom/learn_in_depth/Unit_13 ARM M4 M3/INLINE_ASSEMBLY" -O0 -ffunction-sections -fdata-sections -Wall -gdwarf-2 -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-stm_driver-2f-EXTI
+
+clean-stm_driver-2f-EXTI:
+	-$(RM) ./stm_driver/EXTI/Stm32_F103C6_EXTI_driver.cyclo ./stm_driver/EXTI/Stm32_F103C6_EXTI_driver.d ./stm_driver/EXTI/Stm32_F103C6_EXTI_driver.o ./stm_driver/EXTI/Stm32_F103C6_EXTI_driver.su
+
+.PHONY: clean-stm_driver-2f-EXTI
+
