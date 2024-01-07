87fd7169db8cbbcd58851adfe3d199e0733ed78f
fourat153 <ahmedchammam30@gmail.com>
2024-01-07 13:07:37 +0100

    svc

---
+++ learn_in_depth/Unit_13 ARM M4 M3/SVC_PENDSV/Debug/Src/main.d
@@ -0,0 +1,20 @@
+Src/main.o: ../Src/main.c \
+ ../Src/../stm_driver/inc/Stm32_F103C6_EXTI_driver.h \
+ ../Src/../stm_driver/inc/stm32f103c6.h \
+ ../Src/../stm_driver/inc/Stm32_F103C6_gpio_driver.h \
+ ../Src/../stm_driver/inc/Stm32_F103C6_gpio_driver.h \
+ ../Src/../CMSIS_V5/core_cm3.h ../Src/../CMSIS_V5/ARMCM3.h \
+ ../Src/../CMSIS_V5/core_cm3.h ../Src/../CMSIS_V5/cmsis_version.h \
+ ../Src/../CMSIS_V5/cmsis_compiler.h ../Src/../CMSIS_V5/cmsis_gcc.h \
+ ../Src/../CMSIS_V5/system_ARMCM3.h
+../Src/../stm_driver/inc/Stm32_F103C6_EXTI_driver.h:
+../Src/../stm_driver/inc/stm32f103c6.h:
+../Src/../stm_driver/inc/Stm32_F103C6_gpio_driver.h:
+../Src/../stm_driver/inc/Stm32_F103C6_gpio_driver.h:
+../Src/../CMSIS_V5/core_cm3.h:
+../Src/../CMSIS_V5/ARMCM3.h:
+../Src/../CMSIS_V5/core_cm3.h:
+../Src/../CMSIS_V5/cmsis_version.h:
+../Src/../CMSIS_V5/cmsis_compiler.h:
+../Src/../CMSIS_V5/cmsis_gcc.h:
+../Src/../CMSIS_V5/system_ARMCM3.h:
